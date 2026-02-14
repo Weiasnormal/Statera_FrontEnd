45db5b52d13f8c289c85d09daa6cd2f2108e43aa
Joko Comia <jokocomia05@gmail.com>
2026-02-14 16:08:21 +0800

    feat: update Meet the Team page layout and add background SVG graphics

--- app/(public)/meet_the_team_page/page.tsx
+++ app/(public)/meet_the_team_page/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import TeamHero from "@/app/components/heroes/TeamHero";
-import Navbar from "@/app/components/layout/Navbar";
 import Footer from "@/app/components/layout/footer";
 
 export default function MeetTheTeam() {
